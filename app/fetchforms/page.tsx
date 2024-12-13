67ea5f56acb25b5e2d3563f5d78462ef97e097c3
ricardosantis <rsdsantis@gmail.com>
2024-12-13 16:41:41 -0300

    Complete technical assessment

--- app/fetchforms/page.tsx
+++ app/fetchforms/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import { QueryClientProvider } from '@tanstack/react-query';
+import { useQueryClient ,QueryClientProvider } from '@tanstack/react-query';
 import queryClient from '@/lib/queryClient';
 
 interface Template {
@@ -18,11 +18,8 @@ const fetchTemplates = async (): Promise<Template[]> => {
     return response.json();
 };
 
-interface FetchFormsPageProps {
-    dehydratedState: unknown;
-}
-
-const FetchFormsPage: React.FC<FetchFormsPageProps> = () => {
+const FetchForms: React.FC = () => {
+    const queryClient = useQueryClient();
     const { data: templates, isLoading, isError } = useQuery({
         queryKey: ["templates"],
         queryFn: fetchTemplates,
@@ -37,7 +34,6 @@ const FetchFormsPage: React.FC<FetchFormsPageProps> = () => {
     }
 
     return (
-        <QueryClientProvider client={queryClient}>
             <div>
                 <main>
                     <h1>Saved Forms</h1>
@@ -48,8 +44,13 @@ const FetchFormsPage: React.FC<FetchFormsPageProps> = () => {
                     </ul>
                 </main>
             </div>
-        </QueryClientProvider>
     );
 };
+
+const FetchFormsPage: React.FC = () => (
+        <QueryClientProvider client={queryClient}>
+            <FetchForms />
+        </QueryClientProvider>
+);
 
 export default FetchFormsPage;
